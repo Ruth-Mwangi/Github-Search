7c717644c3c773d25b4ea839fa46e36083a37703
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-04-06 12:21:20 +0300

    Created class for ruth

---
+++ src/app/ruth-class/ruth-user.spec.ts
@@ -0,0 +1,7 @@
+import { RuthUser } from './ruth-user';
+
+describe('RuthUser', () => {
+  it('should create an instance', () => {
+    expect(new RuthUser()).toBeTruthy();
+  });
+});
---
+++ src/app/ruth-class/ruth-user.ts
@@ -0,0 +1,5 @@
+export class RuthUser {
+
+    constructor(public name:string,public avatar_url:string,public repos_url:string){}
+}
+
