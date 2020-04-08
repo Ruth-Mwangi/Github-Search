1b3b35f4f87025f739c2636d4a95b5640a43d576
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-04-09 00:12:58 +0300

    Updated the highlight directive

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,23 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor() { }
+  constructor(private elem:ElementRef) { }
+
+
+  @HostListener ('click') onClicks(){
+    this.textDeco("rgba(0, 0, 255,0.47)");
+  }
+
+  
+  textDeco(deco:string){
+
+    this.elem.nativeElement.style.backgroundColor=deco;
+  }
+
+
 
 }
