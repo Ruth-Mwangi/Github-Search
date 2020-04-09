d823767e4412b05d82d005f3e200983f3c79f51a
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-04-09 22:37:35 +0300

    Updated files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  ruthApiUrl:"https://api.github.com/users/Ruth-Mwangi?access_token=5a940cfd1992f2f5488c4b93117c7f9aa3daef17",
+  rururepo:"https://api.github.com/search/repositories?access_token=5a940cfd1992f2f5488c4b93117c7f9aa3daef17&q=user:Ruth-Mwangi",
+  searchRepo:"https://api.github.com/search/repositories?access_token=5a940cfd1992f2f5488c4b93117c7f9aa3daef17&q=",
+  searchUser:"https://api.github.com/search/users?access_token=5a940cfd1992f2f5488c4b93117c7f9aa3daef17&q="
 };
