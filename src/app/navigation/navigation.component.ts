4651895636a7c5cea7673cd1bf316bfd4c5ec1aa
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-04-09 14:11:27 +0300

    Updated multiple files

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { UserSearchService } from '../search-service/user-search.service';
 import { RepoSearchService } from '../search-service/repo-search.service';
 
@@ -9,14 +9,18 @@ import { RepoSearchService } from '../search-service/repo-search.service';
 })
 export class NavigationComponent implements OnInit {
 
+  @ViewChild('search') elem: ElementRef;
+
   constructor(private userservice:UserSearchService,private reposervice:RepoSearchService) { }
 
   searchRepo(search:HTMLInputElement){
     this.reposervice.searchRepo(search);
+    this.elem.nativeElement.value=''
 
   }
   searchUser(search:HTMLInputElement){
     this.userservice.searchUser(search);
+    this.elem.nativeElement.value=''
     
   }
 
